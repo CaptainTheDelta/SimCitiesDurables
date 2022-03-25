e0fe87738f9191e18211f43c8434647aa5c214ae
CaptainTheDelta <lesecq.damien@gmail.com>
2022-03-25 14:42:35 +0100

    Création de la base du calcul de la note

---
+++ notation.py
@@ -0,0 +1,16 @@
+# Partie dimensionnement
+note_dimmensionnement = 0
+
+
+# Partie pollution
+note_pollution = 0
+
+
+# Partie éclatement
+note_eclatement = 0
+
+
+# Conclusion
+note_finale = (note_dimmensionnement + note_pollution + note_eclatement) / 3
+
+print(note)
