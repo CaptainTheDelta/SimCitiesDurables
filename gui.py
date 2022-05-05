ecb09712079c1d56ac0fa69c9a272a452102c1c9
CaptainTheDelta <lesecq.damien@gmail.com>
2022-05-05 11:57:50 +0200

    Création sommaire d'une fenêtre de jeu

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import ttk
-
+from PIL import ImageTk, Image
+from simcitydurable.Bloc import *
+from simcitydurable.Game import *
 
 # Partie principale
 
@@ -38,7 +40,20 @@ app.grid(column=0,row=0, sticky=(N, W, E, S))
 app.rowconfigure(0, weight=1)
 app.columnconfigure(1, weight=1)
 
+# chargement des images des blocs
 
+img_folder = "utils/img/"
+img = {}
+
+for bloc in Bloc.__subclasses__():
+    bloc = bloc.__name__
+    try:
+        i = ImageTk.PhotoImage(Image.open(img_folder+bloc+".png"))
+    except:
+        print(f"{img_folder+bloc}.png non trouvé")
+    else:
+        img[bloc] = i
+        
 
 # plateau
 
@@ -49,13 +64,59 @@ board.grid(column=1,row=0)
 
 
 
+size = 100 #px
+
+im = Image.new("RGB", (size,size), "white")
+im.save('white.jpg')
+
+vide = ImageTk.PhotoImage(Image.open("white.jpg"))
+
+g = Game()
+
+for (x,y),cell in g.board:
+    c = Canvas(board,height=size,width=size)
+    c.create_image(0,0,anchor=NW,image=img[cell.__class__.__name__])
+    c.grid(row=x,column=y)
+
+
+
 # Bibliothèque de blocs
 
-lib = ttk.Frame(app,width=200)
+lib_frame = ttk.Frame(app)
+lib_frame.grid(column=0, row=0, sticky=(N, W, S),rowspan=2)
+lib_frame.rowconfigure(index=0, weight=1)
+
+lib_canvas = Canvas(lib_frame, background="yellow")
+scrollbar = ttk.Scrollbar(lib_frame, orient="vertical", command=lib_canvas.yview)
+
+lib = ttk.Frame(lib_canvas)
 lib['borderwidth'] = 4
 lib['relief'] = 'sunken'
-lib.grid(column=0, row=0, sticky=(N, W, S), rowspan=2)
+lib.grid(column=0, row=0)
 
+lib.bind(
+    "<Configure>",
+    lambda e: lib_canvas.configure(
+        scrollregion=lib_canvas.bbox("all"),
+        width=e.width
+    )
+)
+
+lib_canvas.create_window((0, 0), window=lib, anchor="nw")
+lib_canvas.configure(yscrollcommand=scrollbar.set)
+
+k = 0
+for bloc in Bloc.__subclasses__():
+    bloc = bloc.__name__
+    if bloc in img:
+        c = Canvas(lib,height=size,width=size)
+        c.create_image(0,0,anchor=NW,image=img[bloc])
+        row,column = divmod(k, 3)
+        c.grid(row=row,column=column)
+        k += 1
+
+lib_canvas.grid(column=0, row=0, sticky=(N, W, S))
+scrollbar.grid(column=1, row=0, sticky=(N, E, S))
 
 
 # Zone d'affichage des scores
