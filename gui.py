7bc3e89c0127642647a503b64687c58e86f936e6
CaptainTheDelta <lesecq.damien@gmail.com>
2022-05-20 14:24:34 +0200

    Mise en forme de l'application sous forme de classe

--- gui.py
+++ gui.py
@@ -4,144 +4,142 @@ from PIL import ImageTk, Image
 from simcitydurable.Bloc import *
 from simcitydurable.Game import *
 
-# Partie principale
 
-root = Tk()
-root.title("SimCityDurable")
-root.attributes('-zoomed', True)
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-
-
-menu_bar = Menu(root)
-
-menu_file = Menu(menu_bar, tearoff=0)
-menu_file.add_command(label="Quitter")
-
-menu_tools = Menu(menu_bar, tearoff=0)
-menu_tools.add_command(label="JSP frero")
-
-menu_about = Menu(menu_bar, tearoff=0)
-menu_about.add_command(label="Aide")
-
-menu_bar.add_cascade(menu=menu_file, label="Fichier")
-menu_bar.add_cascade(menu=menu_tools, label="Outils")
-menu_bar.add_cascade(menu=menu_about, label="A propos")
-
-root.config(menu=menu_bar)
-
-
-s = ttk.Style()
-# Create style used by default for all Frames
-s.configure('app.TFrame', background='white')
-
-app = ttk.Frame(root, style='app.TFrame')
-app.grid(column=0,row=0, sticky=(N, W, E, S))
-app.rowconfigure(0, weight=1)
-app.columnconfigure(1, weight=1)
-
-# chargement des images des blocs
-
-img_folder = "utils/img/"
-img = {}
-
-for bloc in Bloc.__subclasses__():
-    bloc = bloc.__name__
-    try:
-        i = ImageTk.PhotoImage(Image.open(img_folder+bloc+".png"))
-    except:
-        print(f"{img_folder+bloc}.png non trouvé")
-    else:
-        img[bloc] = i
-        
-
-# plateau
-
-board = ttk.Frame(app, width=1000, height=1000)
-board.grid(column=1,row=0)
-
-
+img_folder = "./utils/img/"
 
 size = 100 #px
 
-im = Image.new("RGB", (size,size), "white")
-im.save('white.jpg')
+# Application
 
-vide = ImageTk.PhotoImage(Image.open("white.jpg"))
+class SimCityDurableApp:
+    def __init__(self, root):
+        # configuration de la fenêtre
+        root.title("SimCityDurable")
+        root.attributes('-zoomed', True)
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
 
-g = Game()
+        # menus déroulants
+        menu_bar = Menu(root)
 
-for (x,y),cell in g.board:
-    c = Canvas(board,height=size,width=size)
-    c.create_image(0,0,anchor=NW,image=img[cell.__class__.__name__])
-    c.grid(row=x,column=y)
+        menu_file = Menu(menu_bar, tearoff=0)
+        menu_file.add_command(label="Quitter")
 
+        menu_tools = Menu(menu_bar, tearoff=0)
+        menu_tools.add_command(label="Regénérer")
 
+        menu_about = Menu(menu_bar, tearoff=0)
+        menu_about.add_command(label="Aide")
 
-# Bibliothèque de blocs
+        menu_bar.add_cascade(menu=menu_file, label="Fichier")
+        menu_bar.add_cascade(menu=menu_tools, label="Outils")
+        menu_bar.add_cascade(menu=menu_about, label="A propos")
 
-lib_frame = ttk.Frame(app)
-lib_frame.grid(column=0, row=0, sticky=(N, W, S),rowspan=2)
-lib_frame.rowconfigure(index=0, weight=1)
+        root.config(menu=menu_bar)
 
-lib_canvas = Canvas(lib_frame)
-scrollbar = ttk.Scrollbar(lib_frame, orient="vertical", command=lib_canvas.yview)
+        # fenêtre sous le menu
+        s = ttk.Style()
+        s.configure('app.TFrame', background='white')
 
-lib = ttk.Frame(lib_canvas)
-lib.grid(column=0, row=0)
+        app = ttk.Frame(root, style='app.TFrame')
+        app.grid(column=0,row=0, sticky=(N, W, E, S))
+        app.rowconfigure(0, weight=1)
+        app.columnconfigure(1, weight=1)
 
-lib.bind(
-    "<Configure>",
-    lambda e: lib_canvas.configure(
-        scrollregion=lib_canvas.bbox("all"),
-        width=e.width
-    )
-)
+        # chargement des images des blocs
+        self.img = {}
+        for bloc in Bloc.__subclasses__():
+            bloc = bloc.__name__
+            try:
+                i = ImageTk.PhotoImage(Image.open(img_folder+bloc+".png"))
+            except:
+                print(f"{img_folder+bloc}.png non trouvé")
+            else:
+                self.img[bloc] = i
 
-lib_canvas.create_window((0, 0), window=lib, anchor="nw")
-lib_canvas.configure(yscrollcommand=scrollbar.set)
+        # création d'un jeu
+        self.g = Game()
 
-k = 0
-for bloc in Bloc.__subclasses__():
-    bloc = bloc.__name__
-    if bloc in img:
-        c = Canvas(lib,height=size,width=size)
-        c.create_image(0,0,anchor=NW,image=img[bloc])
-        row,column = divmod(k, 3)
-        c.grid(row=row,column=column)
-        k += 1
+        # plateau
+        self.board = ttk.Frame(app, width=10*size, height=10*size)
+        self.board.grid(column=1,row=0)
 
-lib_canvas.grid(column=0, row=0, sticky=(N, W, S))
-scrollbar.grid(column=1, row=0, sticky=(N, E, S))
-
-def _bound_to_mousewheel(event):
-    lib_frame.bind_all("<Button-4>", _on_mousewheel)
-    lib_frame.bind_all("<Button-5>", _on_mousewheel)
-    lib_frame.bind_all("<Motion>", _on_motion)
-
-def _unbound_to_mousewheel(event):
-    lib_frame.unbind_all("<Button-4>")
-    lib_frame.unbind_all("<Button-5>")
-    lib_frame.unbind_all("<Motion>")
-
-def _on_motion(event):
-    print(event.x, event.y)
-
-from pprint import pprint
-
-def _on_mousewheel(event):
-    i = (event.num-4)*2-1
-    lib_canvas.yview_scroll(i, "units")
+        for (x,y),cell in self.g.board:
+            c = Canvas(self.board,height=size,width=size)
+            c.create_image(0,0,anchor=NW,image=self.img[cell.__class__.__name__])
+            c.grid(row=x,column=y)
 
 
-lib_frame.bind('<Enter>', _bound_to_mousewheel)
-lib_frame.bind('<Leave>', _unbound_to_mousewheel)
+
+        # Bibliothèque de blocs
+        self.lib_frame = ttk.Frame(app)
+        self.lib_frame.grid(column=0, row=0, sticky=(N, W, S),rowspan=2)
+        self.lib_frame.rowconfigure(index=0, weight=1)
+
+        self.lib_canvas = Canvas(self.lib_frame)
+        scrollbar = ttk.Scrollbar(self.lib_frame, orient="vertical", command=self.lib_canvas.yview)
+
+        self.lib = ttk.Frame(self.lib_canvas)
+        self.lib.grid(column=0, row=0)
+
+        self.lib.bind(
+            "<Configure>",
+            lambda e: self.lib_canvas.configure(
+                scrollregion=self.lib_canvas.bbox("all"),
+                width=e.width
+            )
+        )
+
+        self.lib_canvas.create_window((0, 0), window=self.lib, anchor="nw")
+        self.lib_canvas.configure(yscrollcommand=scrollbar.set)
+
+        k = 0
+        for bloc in Bloc.__subclasses__()[4:]:
+            bloc = bloc.__name__
+            if bloc in self.img:
+                c = Canvas(self.lib,height=size,width=size)
+                c.create_image(0,0,anchor=NW,image=self.img[bloc])
+                row,column = divmod(k, 3)
+                c.grid(row=row,column=column)
+                k += 1
+
+        self.lib_canvas.grid(column=0, row=0, sticky=(N, W, S))
+        scrollbar.grid(column=1, row=0, sticky=(N, E, S))
+
+        self.lib_frame.bind('<Enter>', self._bound_to_mousewheel)
+        self.lib_frame.bind('<Leave>', self._unbound_to_mousewheel)
+
+        # Zone d'affichage des scores
+        score = ttk.Label(app, text="Score")
+        score.grid(column=1,row=1, sticky=(N, W, E, S))
+
+        self.app = app
 
 
-# Zone d'affichage des scores
+    def _bound_to_mousewheel(self, event):
+        self.lib_frame.bind_all("<Button-4>", self._on_mousewheel)
+        self.lib_frame.bind_all("<Button-5>", self._on_mousewheel)
+        self.lib_frame.bind_all("<Motion>", self._on_motion)
 
-score = ttk.Label(app, text="Score")
-score.grid(column=1,row=1, sticky=(N, W, E, S))
+
+    def _unbound_to_mousewheel(self, event):
+        self.lib_frame.unbind_all("<Button-4>")
+        self.lib_frame.unbind_all("<Button-5>")
+        self.lib_frame.unbind_all("<Motion>")
 
 
-root.mainloop()
+    def _on_motion(self, event):
+        pass
+
+
+    def _on_mousewheel(self, event):
+        i = (event.num-4)*2-1
+        self.lib_canvas.yview_scroll(i, "units")
+
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    app = SimCityDurableApp(root)
+    root.mainloop()
