bf9aebbe6fd3c99656c086ca45d5b62aab402731
CaptainTheDelta <lesecq.damien@gmail.com>
2022-06-03 03:25:57 +0200

    Modification du jeu en même temps que le plateau, calcul des scores en direct

--- gui.py
+++ gui.py
@@ -9,9 +9,11 @@ img_folder = os.path.join(os.getcwd(),"utils/img")
 
 size = 100 #px
 
+# petit truc pour faire passer des données
+global data
 
 class Cell(Canvas):
-    def __init__(self,bloc,parent, drag, drop, app, board, *args, **kwargs):
+    def __init__(self,bloc,parent, drag, drop, app, board, game, *args, **kwargs):
         super().__init__(parent, width=size,height=size, *args, **kwargs)
         self.create_image(0,0,anchor=NW,image=img[bloc])
         self.bloc = bloc
@@ -19,6 +21,7 @@ class Cell(Canvas):
         self.y = 0
         self.app = app
         self.board = board
+        self.game = game
         
         self.drag = drag
         self.drop = drop
@@ -30,6 +33,7 @@ class Cell(Canvas):
             self["cursor"] = "hand1"
 
     def on_start(self, event):
+        global data
         self.icone = Canvas(self.app, width=size, height=size)
         self.icone.create_image(0,0,anchor=NW,image=img[self.bloc])
         self.x = self.app.winfo_rootx() + event.x
@@ -40,8 +44,14 @@ class Cell(Canvas):
         self.column = self.grid_info()['column']
 
         if self.drop:
-            vide = Cell("Vide", self.board, False, True, self.app, self.board)
+            vide = Cell("Vide", self.board, False, True, self.app, self.board, self.game)
             vide.grid(row=self.row, column=self.column)
+            data = {
+                "row": self.row,
+                "column": self.column,
+                "bloc": "Vide",
+            }
+            self.app.event_generate("<<Update Board>>")
 
 
     def on_drag(self, event):
@@ -50,6 +60,7 @@ class Cell(Canvas):
         self.icone.place(x=xd,y=yd)
 
     def on_drop(self, event):
+        global data
         self.icone.destroy()
         x,y = event.widget.winfo_pointerxy()
         target = event.widget.winfo_containing(x,y)
@@ -58,10 +69,18 @@ class Cell(Canvas):
             row_target = target.grid_info()['row'] 
             column_target = target.grid_info()['column']
 
-            c = Cell(self.bloc, self.board, True, True, self.app, self.board)
+            c = Cell(self.bloc, self.board, True, True, self.app, self.board, self.game)
             c.grid(row=row_target, column=column_target)
+            self.game.board.board[column_target][row_target] = self.bloc
+            data = {
+                "row": row_target,
+                "column": column_target,
+                "bloc": self.bloc,
+            }
         else:
             self.grid(row=self.row, column=self.column)
+        
+        self.app.event_generate("<<Update Board>>")
 
 
 # Application
@@ -69,7 +88,7 @@ class SimCityDurableApp:
     def __init__(self, root):
         # configuration de la fenêtre
         root.title("SimCityDurable")
-        #root.attributes('-zoomed', True)
+        root.attributes('-zoomed', True)
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
 
@@ -80,7 +99,7 @@ class SimCityDurableApp:
         menu_file.add_command(label="Quitter")
 
         menu_tools = Menu(menu_bar, tearoff=0)
-        menu_tools.add_command(label="Regénérer")
+        menu_tools.add_command(label="Regénérer", command=self.reset_game)
 
         menu_about = Menu(menu_bar, tearoff=0)
         menu_about.add_command(label="Aide")
@@ -100,6 +119,7 @@ class SimCityDurableApp:
         self.app.rowconfigure(0, weight=1)
         self.app.columnconfigure(1, weight=1)
 
+        self.app.bind("<<Update Board>>", self._update_score)
 
         # création d'un jeu
         self.g = Game()
@@ -108,13 +128,7 @@ class SimCityDurableApp:
         self.board = ttk.Frame(self.app, width=10*size, height=10*size)
         self.board.grid(column=1,row=0)
 
-        for (x,y),cell in self.g.board:
-            drag = blocs[cell]["catégorie"] != "Indispensable"
-            drop = drag or cell == "Vide"
-            c = Cell(cell,self.board,drag, drop, self.app, self.board)
-            c.grid(row=x,column=y)
-
-
+        self.draw_game()
 
         # Bibliothèque de blocs
         self.lib_frame = ttk.Frame(self.app)
@@ -149,7 +163,7 @@ class SimCityDurableApp:
                     prev_cat = blocs[bloc]["catégorie"]
                     ttk.Label(self.lib, text=prev_cat).grid(row=k//largeur+1,column=0,columnspan=largeur)
                     k = (k // largeur + 2) * largeur
-                c = Cell(bloc,self.lib,True, False, self.app, self.board)
+                c = Cell(bloc,self.lib,True, False, self.app, self.board, self.g)
                 row,column = divmod(k, largeur)
                 c.grid(row=row,column=column)
                 k += 1
@@ -164,47 +178,55 @@ class SimCityDurableApp:
         self.scores_frame = ttk.Frame(self.app)
         self.scores_title = ttk.Label(self.scores_frame, text="Scores")
         self.scores_title.grid(column=0, row=0, sticky=(N, W, E), columnspan=2)
+        self.scores_frame.columnconfigure(0, weight=1)
+        self.scores_frame.columnconfigure(1, weight=1)
         
-        self.score_dimensionnement_label = ttk.Label(self.scores_frame, text="Score dimensionnement")
-        self.score_pollution_label = ttk.Label(self.scores_frame, text="Score pollution")
-        self.score_disposition_label = ttk.Label(self.scores_frame, text="Score disposition")
-        self.score_final_label = ttk.Label(self.scores_frame, text="Score final")
-        self.score_dimensionnement_label.grid(row=1, column=0, sticky=E)
-        self.score_pollution_label.grid(row=2, column=0, sticky=E)
-        self.score_disposition_label.grid(row=3, column=0, sticky=E)
-        self.score_final_label.grid(row=2, column=3, sticky=E)
+        self.scores_frame_left = ttk.Frame(self.scores_frame)
+        self.scores_frame_left.grid(row=1, column=0)
+        self.scores_frame_left.columnconfigure(0, weight=1)
+
+        self.scores_frame_right = ttk.Frame(self.scores_frame)
+        self.scores_frame_right.grid(row=1, column=1)
+        self.scores_frame_right.columnconfigure(0, weight=1)
+
+        ttk.Label(self.scores_frame_left, text="Score dimensionnement : ").grid(row=0, column=0, sticky=E)
+        ttk.Label(self.scores_frame_left, text="Score pollution : ").grid(row=1, column=0, sticky=E)
+        ttk.Label(self.scores_frame_left, text="Score disposition : ").grid(row=2, column=0, sticky=E)
+
+        ttk.Label(self.scores_frame_right, text="Score final : ").grid(row=0, column=0, sticky=E)
 
         self.score_dimensionnement = StringVar(value='0')
         self.score_pollution = StringVar(value='0')
         self.score_disposition = StringVar(value='0')
         self.score_final = StringVar(value='0')
 
-        self.score_dimensionnement_display = ttk.Label(self.scores_frame, textvariable=self.score_dimensionnement, width=4)
-        self.score_pollution_display = ttk.Label(self.scores_frame, textvariable=self.score_pollution, width=4)
-        self.score_disposition_display = ttk.Label(self.scores_frame, textvariable=self.score_disposition, width=4)
-        self.score_final_display = ttk.Label(self.scores_frame, textvariable=self.score_final, width=4)
-        self.score_dimensionnement_display.grid(row=1, column=1, sticky=E)
-        self.score_pollution_display.grid(row=2, column=1, sticky=E)
-        self.score_disposition_display.grid(row=3, column=1, sticky=E)
-        self.score_final_display.grid(row=2, column=4, sticky=E)
+        self.score_dimensionnement_display = ttk.Label(self.scores_frame_left, textvariable=self.score_dimensionnement, width=5)
+        self.score_pollution_display = ttk.Label(self.scores_frame_left, textvariable=self.score_pollution, width=5)
+        self.score_disposition_display = ttk.Label(self.scores_frame_left, textvariable=self.score_disposition, width=5)
+        self.score_final_display = ttk.Label(self.scores_frame_right, textvariable=self.score_final, width=5)
+        self.score_dimensionnement_display.grid(row=0, column=1)
+        self.score_pollution_display.grid(row=1, column=1)
+        self.score_disposition_display.grid(row=2, column=1)
+        self.score_final_display.grid(row=0, column=1)
 
-        self.score_sur_20_1 = ttk.Label(self.scores_frame, text="/20")
-        self.score_sur_20_2 = ttk.Label(self.scores_frame, text="/20")
-        self.score_sur_20_3 = ttk.Label(self.scores_frame, text="/20")
-        self.score_sur_20_4 = ttk.Label(self.scores_frame, text="/20")
-        self.score_sur_20_1.grid(row=1, column=2, sticky=W)
-        self.score_sur_20_2.grid(row=2, column=2, sticky=W)
-        self.score_sur_20_3.grid(row=3, column=2, sticky=W)
-        self.score_sur_20_4.grid(row=2, column=5, sticky=W)
+        ttk.Label(self.scores_frame_left, text="/20").grid(row=0, column=2, sticky=W)
+        ttk.Label(self.scores_frame_left, text="/20").grid(row=1, column=2, sticky=W)
+        ttk.Label(self.scores_frame_left, text="/20").grid(row=2, column=2, sticky=W)
+        ttk.Label(self.scores_frame_right, text="/20").grid(row=0, column=2, sticky=W)
 
-
+        self._update_score('')
         self.scores_frame.grid(row=1, column=1, sticky=(E,S,W))
 
+    def draw_game(self):
+        for (x,y),cell in self.g.board:
+            drag = blocs[cell]["catégorie"] != "Indispensable"
+            drop = drag or cell == "Vide"
+            c = Cell(cell,self.board,drag, drop, self.app, self.board, self.g)
+            c.grid(row=x,column=y)
 
     def _bound_to_mousewheel(self, event):
         self.lib_frame.bind_all("<Button-4>", self._on_mousewheel)
         self.lib_frame.bind_all("<Button-5>", self._on_mousewheel)
-        self.lib_frame.bind_all("<Motion>", self._on_motion)
 
 
     def _unbound_to_mousewheel(self, event):
@@ -213,15 +235,25 @@ class SimCityDurableApp:
         self.lib_frame.unbind_all("<Motion>")
 
 
-    def _on_motion(self, event):
-        pass
-
-
     def _on_mousewheel(self, event):
         i = (event.num-4)*2-1
         self.lib_canvas.yview_scroll(i, "units")
 
-
+    
+    def _update_score(self, event):
+        global data
+        if event != "":
+            self.g.board.board[data["column"]][data["row"]] = data["bloc"]
+        dim,pol,disp = self.g.get_notes()
+        self.score_dimensionnement.set(f"{dim:.2f}")
+        self.score_pollution.set(f"{pol:.2f}")
+        self.score_disposition.set(f"{disp:.2f}")
+        sf = (dim + pol + disp) / 3
+        self.score_final.set(f"{sf:.2f}")
+    
+    def reset_game(self):
+        self.g = Game()
+        self.draw_game()
 
 if __name__ == "__main__":
     root = Tk()
